192aaca1ef584cf500751c6e3c7d6a5af85eee7a
Sambhav Saxena <sambhavsaxena02@gmail.com>
2022-09-24 18:31:49 +0530

    added giturl and tables

--- App.js
+++ App.js
@@ -1,18 +1,23 @@
 import { useState, useEffect } from 'react';
-import { StyleSheet, Text, SafeAreaView, FlatList, ActivityIndicator, Alert } from 'react-native';
+import { StyleSheet, Text, SafeAreaView, FlatList, ActivityIndicator, Alert, Linking, Button } from 'react-native';
 
 const url = "https://codeforces.com/api/contest.list?gym=false";
+const giturl = "https://github.com/sambhavsaxena"
 const App = () => {
   const [isLoading, setLoading] = useState(true);
   const [data, setData] = useState([]);
   useEffect(() => {
     fetch(url)
       .then((response) => response.json())
-      .then((json) => setData(json.result.slice(0, 25)))
+      .then((json) => setData(json.result.slice(0, 30)))
       .catch((error) => Alert.alert("Error", error))
+      .then(() => Alert.alert("Contests Updated", "The upcoming contests have been updated."))
       .finally(() => setLoading(false))
-      .then(() => Alert.alert("Contests fetched successfully", "Alarm for the upcoming contests has been set!"))
   }, []);
+
+  const handlepress = () => {
+    Linking.openURL(giturl);
+  }
 
   return (
     <SafeAreaView style={styles.container}>
@@ -31,21 +36,32 @@ const App = () => {
               var hours = date.getHours();
               var minutes = date.getMinutes();
               return (
-                <Text style={styles.text}>
-                  {item.id}. {item.name}
-                  {"\n"}
-                  <Text style={styles.text2}>
-                    {dateStr}, {hours} hrs {minutes} mins
+                date > new Date() ?
+                  <Text style={styles.table}>
+                    <Text style={styles.text}>
+                      {item.id}. {item.name} | <Text style={styles.none1}>Upcoming Contests</Text>
+                      {"\n"}
+                      <Text style={styles.text2}>
+                        {dateStr}, {hours} hrs {minutes} mins
+                      </Text>
+                    </Text>
+                  </Text> : <Text style={styles.table}>
+                    <Text style={styles.text}>
+                      {item.id}. {item.name} | <Text style={styles.none2}>Contest Over</Text>
+                      {"\n"}
+                      <Text style={styles.text2}>
+                        {dateStr}, {hours} hrs {minutes} mins
+                      </Text>
+                    </Text>
                   </Text>
-                </Text>
               )
             }}
           />
       }
-      <Text style={styles.footer}>
-        By Sambhav Saxena
+      <Text style={styles.footer} onPress={handlepress}>
+        With love, Sambhav Saxena
       </Text>
-    </SafeAreaView>
+    </SafeAreaView >
   );
 }
 
@@ -54,6 +70,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#000',
     alignItems: 'center',
+    marginTop: 30,
     justifyContent: 'center',
   },
   text: {
@@ -68,14 +85,30 @@ const styles = StyleSheet.create({
   title: {
     textDecoration: 'underline',
     color: '#fff',
-    marginTop: 50,
-    marginBottom: 20,
+    marginTop: 30,
+    marginBottom: 30,
     fontFamily: 'monospace',
     fontSize: 20,
   },
   footer: {
     color: '#fff',
+    margin: 16,
+  },
+  none1: {
+    color: 'green'
+  },
+  none2: {
+    color: 'yellow'
+  },
+  table: {
+    backgroundColor: '#000',
+    color: '#fff',
+    fontFamily: 'monospace',
+    padding: 10,
     margin: 10,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: '#fff',
   }
 });
 
